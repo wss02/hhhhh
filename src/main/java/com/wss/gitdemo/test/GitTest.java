7a8f9d5f3630c1199d97fe55a14ae95afa1fce67
wss <10086@qq.com>
2021-08-15 15:09:47 +0800

    refactor(test) git-test

--- src/main/java/com/wss/gitdemo/test/GitTest.java
+++ src/main/java/com/wss/gitdemo/test/GitTest.java
@@ -3,5 +3,6 @@ package com.wss.gitdemo.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hard working，hard luckier");
+        System.out.println("Winner is King");
     }
 }
